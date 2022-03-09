a4fe20b7ab2de202a36999345f762417ab4f0d37
chanhong-ji <jichanhong24@gmail.com>
2022-03-09 20:51:32 +0900

    feat: Add header
    Two options in header with TouchableOpacity

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { useState } from "react";
+import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState({});
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => setWorking(true)}>
+          <Text style={{ ...styles.option, color: working ? "white" : "grey" }}>
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setWorking(false)}>
+          <Text
+            style={{ ...styles.option, color: !working ? "white" : "grey" }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.main}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, backgroundColor: theme.bg },
+
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    paddingHorizontal: 30,
   },
+  option: {
+    fontSize: 40,
+    color: theme.title,
+  },
+
+  main: { flex: 5 },
 });
